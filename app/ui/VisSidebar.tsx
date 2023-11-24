05ea90b5e6b6d4344ce000225b51b4ce199f22f9
totoro <shockterraz@gmail.com>
2023-11-25 01:12:18 +0700

    Clear Tag Button

--- app/ui/VisSidebar.tsx
+++ app/ui/VisSidebar.tsx
@@ -7,14 +7,21 @@ import { Inter } from "next/font/google";
 import { IoLibrary } from "react-icons/io5";
 import { VscSettings } from "react-icons/vsc";
 import { SidebarTag, SidebarTagRm } from "./SidebarTag";
+import { RiProhibitedLine } from "react-icons/ri";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function VisSidebar() {
   const [activeTagList, setActiveTagList] = useState<string[]>([]);
+  //Show applied Tags
+  // const [showSidebar, setShowSidebar] = useState<Boolean>(true)
+
+  // const showSidebarClass = showSidebar? "left-0 w-80" : "-left-96"
+
+  const libraryList = ["d3.js", "altair", "vega"];
 
   return (
-    <aside className="border-r p-6 h-screen bg-white z-20 fixed -left-96 lg:left-0 lg:w-80  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+    <aside className="border-r p-6 h-screen bg-white z-20 -left-96 lg:left-0 lg:w-80 fixed peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
       <div className="flex flex-col justify-start item-center">
         <div className="pb-4 border-b w-full transition-all duration-300">
           <div className="flex flex-row items-center justify-start gap-x-2">
@@ -22,7 +29,7 @@ export default function VisSidebar() {
             <p className="text-base font-semibold">Applied Filters</p>
           </div>
           <div className="pt-2">
-            <div>
+            <div className="flex flex-row flex-wrap gap-2">
               {activeTagList.map((eachTag, i) => (
                 <SidebarTagRm
                   label={eachTag}
@@ -32,6 +39,20 @@ export default function VisSidebar() {
                 />
               ))}
             </div>
+          </div>
+          <div>
+            {activeTagList.length > 0 && (
+              <div
+                className="text-red-500 font-semibold flex justify-center
+                 bg-red-200 rounded-md py-2 my-2 cursor-pointer hover:bg-red-200/80 transition-colors delay-50"
+                onClick={() => setActiveTagList([])}
+              >
+                <div className="flex flex-row gap-2 items-center">
+                  <RiProhibitedLine className="text-2xl" />
+                  Reset Filters
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <div className="my-4 border-b pb-4">
@@ -50,11 +71,14 @@ export default function VisSidebar() {
               startContent={<IoLibrary className="text-xl" />}
             >
               <div className="flex flex-row flex-wrap gap-2">
-                <SidebarTag
-                  label="vega"
-                  activeTagList={activeTagList}
-                  setActiveTagList={setActiveTagList}
-                />
+                {libraryList.map((eachTag, i) => (
+                  <SidebarTag
+                    key={i}
+                    label={eachTag}
+                    activeTagList={activeTagList}
+                    setActiveTagList={setActiveTagList}
+                  />
+                ))}
               </div>
             </AccordionItem>
             <AccordionItem
